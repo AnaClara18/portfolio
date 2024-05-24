edcc9d7d42e1db23872b66374600b8604677e45e
Ana Clara <anaclara.aslima@gmail.com>
2024-05-24 17:53:32 -0300

    Adicionando imagens, mexendo na página historia do estágio

--- src/html/componentes/estagio/estagio.js
+++ src/html/componentes/estagio/estagio.js
@@ -1,15 +1,15 @@
-export class Estagio extends HTMLElement{
-    
-    constructor(){
+export class Estagio extends HTMLElement {
+
+    constructor() {
         super();
 
-        console.log ("Constructor estagio.js")
+        console.log("Constructor estagio.js")
 
         console.log(`URL do estagio.js: ${import.meta.url}`)
         fetch('./componentes/estagio/estagio.html').then(resultado => {
 
             resultado.text().then(texto_pagina => {
-            
+
                 let template = document.createElement('template');
 
                 template.innerHTML = texto_pagina;
@@ -17,12 +17,49 @@ export class Estagio extends HTMLElement{
                 this.appendChild(template.content.cloneNode(true));
 
                 this.dispatchEvent(new CustomEvent("carregou"));
+
+                this.initAdditionalFeatures();
+
             });
         });
 
 
 
     }
+
+    initAdditionalFeatures() {
+        console.log("Inicializando funcionalidades adicionais...");
     
-   
+        // Código adicional
+        this.count = 1;
+        const radio1 = this.querySelector("#radio1");
+    
+        if (radio1) {
+            radio1.checked = true;
+    
+            setInterval(() => {
+             this.nextImage();
+            }, 9000);
+        }
+        console.log("O código vem ate aqui 1!");
+    }
+    
+    nextImage() {
+        console.log("O código vem ate aqui 2!");
+        this.count++;
+        if (this.count > 4) {
+            this.count = 1;
+        }
+        console.log("O código vem ate aqui 3!");
+    
+        const radio = this.querySelector("#radio" + this.count);
+        if (radio) {
+            radio.checked = true;
+        }
+        onsole.log("O código vem ate aqui 4!");
+
+    }
+
 }
+
+customElements.define('br-estagio', Estagio);
